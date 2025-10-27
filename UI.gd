188068cdd98db87962143a3eeac4fe09ef0f289f
Sylesh <syleshkumar08@gmail.com>
2025-10-27 18:10:01 -0500

    Pre-restructure backup - working pixel physics v0.1

--- UI.gd
+++ UI.gd
@@ -1,44 +1,23 @@
-# UI.gd - Attach this to a Control node or CanvasLayer
+# UI.gd - Simple UI overlay (most UI is now handled by Main.gd)
 extends Control
 
-var main_scene: Node2D
-var element_names = {
-	0: "Empty",
-	1: "Sand", 
-	2: "Water",
-	3: "Lava",
-	4: "Stone",
-	5: "Fire"
-}
-
 func _ready():
-	# Find the main scene
-	main_scene = get_node("../Main")  # Adjust path as needed
-	
-	# Create UI elements
-	create_ui()
+	# Create simple instructions label
+	create_instructions()
 
-func create_ui():
-	# Instructions label
+func create_instructions():
+	# Instructions label - just basic controls now
 	var instructions = Label.new()
 	instructions.text = """CONTROLS:
 Left Click: Place Element
-2: Water
-3: Lava
+2: Water (hotkey)
+3: Lava (hotkey)
 C: Clear Screen
 
-Current: Sand"""
+Use dropdown to select elements"""
 	instructions.position = Vector2(10, 10)
 	instructions.add_theme_color_override("font_color", Color.WHITE)
 	instructions.add_theme_color_override("font_shadow_color", Color.BLACK)
 	instructions.add_theme_constant_override("shadow_offset_x", 1)
 	instructions.add_theme_constant_override("shadow_offset_y", 1)
 	add_child(instructions)
-
-func _process(_delta):
-	if main_scene and has_node("Label"):
-		var label = get_node("Label")
-		var current_element_name = element_names.get(main_scene.current_element, "Unknown")
-		var text_lines = label.text.split("\n")
-		text_lines[-1] = "Current: " + current_element_name
-		label.text = "\n".join(text_lines)
